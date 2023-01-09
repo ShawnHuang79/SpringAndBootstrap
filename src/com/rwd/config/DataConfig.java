3279ea25acd22c6d7b7c2c2cd5ce4d573e922e76
Shawn Huang <shawn.huang79@gmail.com>
2023-01-09 13:10:46 +0800

    fix settings.properties
    解決更改問題

---
+++ src/com/rwd/config/DataConfig.java
@@ -0,0 +1,57 @@
+package com.rwd.config;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+
+import org.hsqldb.util.DatabaseManagerSwing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+@Configuration
+public class DataConfig {
+	
+	@Bean
+	/*public DataSource getDataSource() {
+		return new EmbeddedDatabaseBuilder()
+				.setType(EmbeddedDatabaseType.HSQL)
+			//	.setName(databaseName)
+				.addScript("resources/schema.sql")
+				.build();
+	}*/
+	public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/advanced4");
+        dataSource.setUsername("root");
+        dataSource.setPassword("1234");
+
+        return dataSource;
+    }
+	@Bean
+	public JdbcOperations jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
+	}
+
+	@Bean
+	public NamedParameterJdbcTemplate getNamedParameterJdbcTemplate() {
+		return new NamedParameterJdbcTemplate(getDataSource());
+	}
+
+//	@PostConstruct
+	/*public void startDBManager() {
+		DatabaseManagerSwing.main(
+				new String[] { "--url", "jdbc:hsqldb:mem:testdb", "--user", "sa", "--password", "" }
+		);
+	}*/
+//	public void startDBManager() {
+//		DatabaseManagerSwing.main(
+//				new String[] { "--url", "jdbc:mysql://localhost:3306/advanced4", "--user", "root", "--password", "1234" }
+//		);
+//	}
+}
